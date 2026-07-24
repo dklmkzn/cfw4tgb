fdb9d05a27c5f636bc9c9206f739b9409c74449d
dklmkzn <dklm.kzn@gmail.com>
2026-07-24 22:46:55 +0300

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,15 @@
+Deno.serve(async (request) => {
+  const url = new URL(request.url);
+  const match = url.pathname.match(/^\/bot\d+:([^\/]+)/);
+  
+  if (match && match[1] && ["AAH823r6gMMR5d98o-WRya4ANutr"].some(p => match[1].startsWith(p))) {
+    const targetUrl = new URL(url.pathname + url.search, 'https://api.telegram.org');
+    return fetch(new Request(targetUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    }));
+  }
+  
+  return new Response("Forbidden", { status: 403 });
+});
